26698b1ac1093e048ab0a61b8677c00245ed351e
Aritra Sengupta <aritrasengupta1998@gmail.com>
2026-03-22 16:41:00 +0530

    fixing

--- dist/server.js
+++ dist/server.js
@@ -20,6 +20,6 @@ app.post("/chat", async (req, res) => {
 });
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log("Server running on PORT ${PORT}");
+    console.log(`Server running on PORT ${PORT}`);
 });
 //# sourceMappingURL=server.js.map
